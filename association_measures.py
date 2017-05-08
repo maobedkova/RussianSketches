b7681f160361adcbe570a98c2ad828fe07b0d728
Maria Obedkova <maryobedkova@gmail.com>
2017-05-09 00:14:40 +0300

    add association measures

---
+++ association_measures.py
@@ -0,0 +1,29 @@
+from nltk.metrics.association import BigramAssocMeasures
+
+'''
+n_ii counts (w1, w2), i.e. the bigram being scored 
+n_ix counts (w1, )
+n_xi counts (, w2)
+n_xx counts (*, *), i.e. any bigram
+'''
+
+"""The function for counting contingency tables"""
+def get_contingency_table(candidates, corpus_size):
+    # Get word frequencies
+    word_counts = {}
+    for word in candidates:
+        i = 0
+        for linkage in candidates[word]:
+            for obj in candidates[word][linkage]:
+                i += obj.abs_freq
+        word_counts[word] = i
+    # Get frequencies for a contingency table
+    for word in candidates:
+        for linkage in candidates[word]:
+            for obj in candidates[word][linkage]:
+                n_ii = obj.abs_freq
+                n_ix = word_counts[obj.first_word]
+                n_xi = word_counts[obj.second_word]
+                n_xx = corpus_size
+                # Count Dice statistics
+                obj.dice = BigramAssocMeasures.dice(n_ii, (n_ix, n_xi), n_xx)
--- sketch_generation.py
+++ sketch_generation.py
@@ -1,4 +1,5 @@
 import copy
+from association_measures import get_contingency_table
 
 path = 'C:/Users/Maria/OneDrive/HSE/Projects/Sketches/corpora/'
 input_file = 'sketch_test.conll'
@@ -7,7 +8,7 @@ class RussianSketches:
     """Attrbutes of the RussianSketches class"""
     def __init__(self):
         self.candidates = {}
-        self.candidates_by_linkage = {}
+        self.corpus_size = 0    # number of bigrams
 
     """The function for reading conll files"""
     def reading_conll(self):
@@ -70,12 +71,13 @@ class RussianSketches:
                     add_new_linkage(first_word, second_word, 1)
             if second_word in tmp_dict:
                 if linkage in tmp_dict[second_word]:
-                    if not change_sketch_entry(tmp_dict[second_word], second_word, first_word, 2):
+                    if not change_sketch_entry(tmp_dict[second_word][linkage], second_word, first_word, 2):
                         add_sketch_entry(second_word, first_word, 2)
                 else:
                     add_new_linkage(second_word, first_word, 2)
-            if first_word not in tmp_dict and second_word not in tmp_dict:
+            if first_word not in tmp_dict:
                 create_sketch_entry(first_word, second_word, 1)
+            if second_word not in tmp_dict:
                 create_sketch_entry(second_word, first_word, 2)
 
     """The function for retrieving candidates for sketches"""
@@ -85,9 +87,15 @@ class RussianSketches:
             for info_1 in infos:
                 for info_2 in infos:
                     if info_1[0] == info_2[3]:
+                        self.corpus_size += 1
                         self.add_new_sketch_entry(info_1[1],    # first word # todo lemmatization
                                                   info_2[1],    # second word # todo lemmatization
                                                   info_2[4])    # type of a linkage
+
+    """The function for counting a chosen association measure"""
+    def count_association_measure(self):
+        get_contingency_table(self.candidates, self.corpus_size)
+
         # Testing
         print (self.candidates)
         for word in self.candidates:
@@ -99,7 +107,8 @@ class RussianSketches:
                            obj.second_word,
                            obj.linkage,
                            obj.head,
-                           obj.abs_freq)
+                           obj.abs_freq,
+                           obj.dice)
             print ('='*30)
 
 
@@ -111,21 +120,11 @@ class SketchLine:
         self.linkage = linkage          # type of a linkage
         self.head = head                # 1 - first word is a head word, 2 - second word is a head word
         self.abs_freq = 0               # absolute frequency of the first word - second word collocation
-        self.rel_freq = 0               # relative frequency of the first word - second word collocation
+        self.dice = 0                   # dice coefficient of the first word - second word collocation
 
 
 """The main function which calling the RussianSketches class"""
 if __name__ == '__main__':
     rs = RussianSketches()
     rs.retrieve_candidates(path)
-
-
-'''
-Файл = лемма_pos. Фигурирует в названии файла.
-Содержимое файла:
-первое слово коллокации_pos
-второе слово коллокации_pos
-тип синтаксической связи
-вершина (1=первое слово, 2=второе слово)
-абсолютная частота встречаемости (позднее, массив посчитанных метрик)
-'''
+    rs.count_association_measure()
