bf54c5d523aa61dc8291c28faf1d2689a0404eba
Maria Obedkova <maryobedkova@gmail.com>
2017-05-20 03:10:20 +0300

    debug

--- parser_comparator.py
+++ parser_comparator.py
@@ -64,81 +64,63 @@ def compare_parsers(golden_standard_file, udpipe_file, syntaxnet_file, rusyntax_
         accuracy.append(float(rel) / len(gs_arr))
         return true, false, accuracy, accuracy_rel
 
-    def find_equivalent_line(w, n, mark, first_line_mark, sp_line, gs_line, sp_arr, gs_arr, true, false, accuracy, accuracy_rel):
+    def find_equivalent_line(w, n, mark, sp_line, gs_line, sp_arr, gs_arr, true, false, accuracy, accuracy_rel):
         """The function for finding equivalent lines in the golden standard and in a parser output"""
         gs_splitted = gs_line.strip().split('\t')
         sp_splitted = sp_line.strip().split('\t')
-        # print (gs_splitted, sp_splitted, gs, sp)
         if gs_splitted[1] == sp_splitted[1] and gs_splitted[0] == sp_splitted[0]:
-            w.write ('GOT! ' + gs_splitted[1] + ' ' + sp_splitted[1])
+            w.write ('GOT! ' + gs_splitted[1] + ' ' + sp_splitted[1] + '\n')
             gs_arr.append(gs_splitted[6])
             sp_arr.append(sp_splitted[6])
             n = 1
-            mark += 1
-        #     first_line_mark = 0
-        # else:
-        #     if first_line_mark == 1:
-        #         first_line_mark = 2
-        return n, mark, first_line_mark, gs_arr, sp_arr, true, false, accuracy, accuracy_rel
+            mark = 1
+        else:
+            mark = 0
+        return n, mark, gs_arr, sp_arr, true, false, accuracy, accuracy_rel
 
-    def writing_check(w, gs_line, gs_arr, sp_arr, true, false, accuracy, accuracy_rel, mark):
-        if len(gs_line) == 1:
+    def writing_check(w, gs_line, gs_arr, sp_arr, true, false, accuracy, accuracy_rel):
+        if gs_line.split('\t')[0] == '1':
             if gs_arr != []:
                 true, false, accuracy, accuracy_rel = count_accuracy(gs_arr, sp_arr,
                                                             true, false,
                                                             accuracy, accuracy_rel)
-                w.write (str(true) + ' ' + str(false) + ' ' + str(accuracy))
+                w.write (str(true) + ' ' + str(false) + ' ' + str(accuracy) + '\n')
                 return True
 
+    def flow(parser_file):
+        for line in parser_file:
+            if len(line) == 1:
+                break
 
-    def iter_file(w, i, first_line_mark, parser_file, n, mark, gs_line,
+    def iter_file(w, parser_file, n, mark, gs_line,
                   arr, gs_arr, true, false, accuracy, accuracy_rel):
         """The function for iterating a parser file"""
-        if writing_check(w, gs_line, gs_arr, arr, true, false, accuracy, accuracy_rel, mark):
-            mark += 1
-            return n, mark, first_line_mark, gs_arr, arr, true, false, accuracy, accuracy_rel
-
+        if writing_check(w, gs_line, gs_arr, arr, true, false, accuracy, accuracy_rel):
+            arr = []
+            gs_arr = []
         for line in parser_file:
-            # if first_line_mark == 2:
-            #     break
             if not line.startswith('#'):
                 if not len(line) == 1:
-                    n, mark, first_line_mark,\
+                    w.write(line)
+                    n, mark,\
                     gs_arr, arr, \
                     true, false, \
                     accuracy, accuracy_rel = \
-                        find_equivalent_line(w, n, mark,first_line_mark,
+                        find_equivalent_line(w, n, mark,
                                              line, gs_line,
                                              arr, gs_arr,
                                              true, false,
                                              accuracy, accuracy_rel)
-            # if i > 350:
-            #     w.write('!!!!' + str(first_line_mark) + ' !!!!')
-            # w.write(str(first_line_mark))
-            if len(gs_line) != 1 and len(line) == 1:
-                # w.write('EMPTY!')
-                # if first_line_mark > 1:
-                #     arr = []
-                #     first_line_mark = 0
-                #     break
-                # elif gs_line.split('\t')[0] == '1':
-                #     # w.write('@MARK@')
-                #     first_line_mark += 1
-                if gs_line.split('\t')[0] != '1':
-                    # w.write('YEEEP')
-                    arr = []
-                    break
-                # if gs_line.split('\t')[0] == '1':
-                #     first_line_mark = 1
-                #     arr = []
-                #     break
 
+            if len(line) == 1 and len(gs_line) != 1:
+                if gs_line.split('\t')[0] != '1':
+                    arr = []
+                    gs_arr = []
+                    break
             if n == 1:
                 n = 0
                 break
-            i += 1
-
-        return n, mark, first_line_mark, gs_arr, arr, true, false, accuracy, accuracy_rel
+        return n, mark, gs_arr, arr, true, false, accuracy, accuracy_rel
 
     # Different UDpipe scores
     ud_true = 0
@@ -168,8 +150,11 @@ def compare_parsers(golden_standard_file, udpipe_file, syntaxnet_file, rusyntax_
 
     n = 0
     quit_mark = 0
-    first_line_mark = 0
     i = 0
+
+    rs_mark = 0
+    sn_mark = 0
+    ud_mark = 0
 
     # Opening parser`s files
     ud_file = open(path + udpipe_file,  'r', encoding='utf-8')
@@ -181,28 +166,37 @@ def compare_parsers(golden_standard_file, udpipe_file, syntaxnet_file, rusyntax_
         # Opening the golden standard
         with open(path + golden_standard_file, 'r', encoding='utf-8') as gs_file:
             for gs_line in gs_file:
-                mark = 0
                 if quit_mark == 1:
                     if len(gs_line) == 1:
+                        if ud_mark == 1:
+                            flow(ud_file)
+                        if sn_mark == 1:
+                            flow(sn_file)
+                        if rs_mark == 1:
+                            flow(rs_file)
                         quit_mark = 0
+                    continue
+                if len(gs_line) == 1:
                     continue
                 w.write ('==NEW GS==' + gs_line)
                 # Comparison of the golden standard with UDpipe
                 w.write ('UD!')
-                n, mark, first_line_mark, gs_arr, ud_arr, ud_true, ud_false, ud_accuracy, ud_accuracy_rel = \
-                    iter_file(w, i, first_line_mark, ud_file, n, mark, gs_line, ud_arr, gs_ud_arr,
+                n, ud_mark, gs_ud_arr, ud_arr, ud_true, ud_false, ud_accuracy, ud_accuracy_rel = \
+                    iter_file(w, ud_file, n, ud_mark, gs_line, ud_arr, gs_ud_arr,
                               ud_true, ud_false, ud_accuracy, ud_accuracy_rel)
                 # Comparison of the golden standard with SyntaxNet
                 w.write ('SN!')
-                n, mark,first_line_mark, gs_arr, sn_arr, sn_true, sn_false, sn_accuracy, sn_accuracy_rel = \
-                    iter_file(w,i, first_line_mark, sn_file, n, mark, gs_line, sn_arr, gs_sn_arr,
+                n, sn_mark, gs_sn_arr, sn_arr, sn_true, sn_false, sn_accuracy, sn_accuracy_rel = \
+                    iter_file(w, sn_file, n, sn_mark, gs_line, sn_arr, gs_sn_arr,
                               sn_true, sn_false, sn_accuracy, sn_accuracy_rel)
                 # Comparison of the golden standard with RuSyntax
                 w.write ('RS!')
-                n, mark, first_line_mark, gs_arr, rs_arr, rs_true, rs_false, rs_accuracy, rs_accuracy_rel = \
-                    iter_file(w,i, first_line_mark, rs_file, n, mark, gs_line, rs_arr, gs_rs_arr,
+                n, rs_mark, gs_rs_arr, rs_arr, rs_true, rs_false, rs_accuracy, rs_accuracy_rel = \
+                    iter_file(w, rs_file, n, rs_mark, gs_line, rs_arr, gs_rs_arr,
                               rs_true, rs_false, rs_accuracy, rs_accuracy_rel)
-                if mark < 3:
+                # if some of parsers have different tokenization examples are not considered
+                #
+                if (ud_mark + sn_mark + rs_mark) < 3:
                     gs_ud_arr = []
                     gs_sn_arr = []
                     gs_rs_arr = []
@@ -211,7 +205,8 @@ def compare_parsers(golden_standard_file, udpipe_file, syntaxnet_file, rusyntax_
                     rs_arr = []
                     quit_mark = 1
                 i += 1
-                w.write (str(i))
+                if i == 500:
+                    break
 
 
         w.write('The number of sentences processed: ' + str(len(ud_accuracy)) + '\n')
